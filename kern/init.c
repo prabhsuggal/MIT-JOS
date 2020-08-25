1952f5d3505dcc25ba18e6dc58ef95ef826d6dfb
prabhsimrandeep singh <psuggal@gmail.com>
2020-08-25 11:03:06 +0530

    changes for lab 1

--- kern/init.c
+++ kern/init.c
@@ -34,6 +34,7 @@ i386_init(void)
 	cons_init();
 
 	cprintf("6828 decimal is %o octal!\n", 6828);
+	cprintf("[32;45m395[31;40m decimal [37mis %o octal!\n", 395);
 
 	// Test the stack backtrace function (lab 1 only)
 	test_backtrace(5);
